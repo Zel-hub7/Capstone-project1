9dd753b084bafde08a7324d5dac1ff1ccb0052c0
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-29 11:45:13 -0700

    Modify files

---
+++ script.js
@@ -0,0 +1,20 @@
+const speakersData = [
+    {
+      id: '01',
+      name: 'Yochai Benkler',
+      role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+      description:
+        'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
+      featuredImage: 'images/speaker_01.png',
+    },
+  
+    {
+      id: '02',
+      name: 'SohYeong Noh',
+      role: 'Director of Art Centre Nabi and a board member of CC Korea',
+      description:
+        'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+      featuredImage: 'images/speaker_02.png',
+    },
+  ];
+  
