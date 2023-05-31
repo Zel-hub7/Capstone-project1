1d8819cbd99c4cee86a03cd21d6997362a87ef95
zel-hub7 <zelalemyohannes62@gmail.com>
2023-05-31 01:16:27 -0700

    Modify files

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ ham.addEventListener('click', () => {
   hide.style.display = 'block';
   ham.style.display = 'none';
   ham.classList.add('remove');
- 
 });
 
 hide.addEventListener('click', () => {
@@ -28,31 +27,29 @@ links.forEach((link) => {
   });
 });
 
-
-
 const speakersData = [
-    {
-      id: '01',
-      name: 'Yochai Benkler',
-      role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-      description:
+  {
+    id: '01',
+    name: 'Yochai Benkler',
+    role: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    description:
         'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006 ',
-      featuredImage: 'Img/speaker_01 1 (1).svg',
-    },
-  
-    {
-      id: '02',
-      name: 'SohYeong Noh',
-      role: 'Director of Art Centre Nabi and a board member of CC Korea',
-      description:
-        'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
-      featuredImage: 'Img/speaker_01 1 (2).svg',
-    },
-  ];
+    featuredImage: 'Img/speaker_01 1 (1).svg',
+  },
 
-  const speaker = document.querySelector('.featured-speakers');
-  
-  let output = ' ';
+  {
+    id: '02',
+    name: 'SohYeong Noh',
+    role: 'Director of Art Centre Nabi and a board member of CC Korea',
+    description:
+        'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    featuredImage: 'Img/speaker_01 1 (2).svg',
+  },
+];
+
+const speaker = document.querySelector('.featured-speakers');
+
+let output = ' ';
 speakersData.forEach((speaker) => {
   output += `
       <div class="grid">
@@ -77,48 +74,48 @@ speakersData.forEach((speaker) => {
 speaker.innerHTML = output;
 
 const speakersData2 = [
-    {
-      id: '03',
-      name: 'Kilnam Chon',
-      role: '',
-      description:
+  {
+    id: '03',
+    name: 'Kilnam Chon',
+    role: '',
+    description:
         'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame ',
-      featuredImage: 'Img/speaker_04.png',
-    },
-  
-    {
-      id: '04',
-      name: 'Julia Leda',
-      role: 'President of Young Pirates of Europe',
-      description:
+    featuredImage: 'Img/speaker_04.png',
+  },
+
+  {
+    id: '04',
+    name: 'Julia Leda',
+    role: 'President of Young Pirates of Europe',
+    description:
         'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
-      featuredImage: 'img/speaker_05.png',
-    },
-  
-    {
-      id: '05',
-      name: 'Lila tretikov',
-      role: 'Executive Director of the Wikimedia Foundation',
-      description:
+    featuredImage: 'img/speaker_05.png',
+  },
+
+  {
+    id: '05',
+    name: 'Lila tretikov',
+    role: 'Executive Director of the Wikimedia Foundation',
+    description:
         'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.',
-      featuredImage: 'img/speaker_01 1 (3).svg',
-    },
-  
-    {
-      id: '06',
-      name: 'Ryan Merkley',
-      role: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
-      description:
+    featuredImage: 'img/speaker_01 1 (3).svg',
+  },
+
+  {
+    id: '06',
+    name: 'Ryan Merkley',
+    role: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
+    description:
         'Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment',
-      featuredImage: 'img/speaker_06.png',
-    },
-  ];
-  
-  const speaker2 = document.querySelector('.featured-speakers2');
-  
-  let output2 = ' ';
-  speakersData2.forEach((speaker2) => {
-    output2 += `
+    featuredImage: 'img/speaker_06.png',
+  },
+];
+
+const speaker2 = document.querySelector('.featured-speakers2');
+
+let output2 = ' ';
+speakersData2.forEach((speaker2) => {
+  output2 += `
         <div class="grid">
           <img src="${speaker2.featuredImage}" alt="first speaker">
           <ul>
@@ -136,15 +133,14 @@ const speakersData2 = [
           </ul>
         </div>  
         `;
-  });
-  
-  speaker2.innerHTML = output2;
-  
-  const MoreSpeakers = document.querySelector('.featured-speakers2');
-  const MoreBtn = document.querySelector('.more');
-  
-  MoreBtn.addEventListener('click', () => {
-    MoreBtn.classList.toggle('active');
-    MoreSpeakers.classList.toggle('active');
-  });
- 
+});
+
+speaker2.innerHTML = output2;
+
+const MoreSpeakers = document.querySelector('.featured-speakers2');
+const MoreBtn = document.querySelector('.more');
+
+MoreBtn.addEventListener('click', () => {
+  MoreBtn.classList.toggle('active');
+  MoreSpeakers.classList.toggle('active');
+});
